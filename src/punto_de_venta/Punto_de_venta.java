458fcfb0b7e51746cfbf34df18b53adad1102fd1
EliMx <selene.psesma@gmail.com>
2020-03-14 21:09:28 -0700

    Version 1

---
+++ src/punto_de_venta/Punto_de_venta.java
@@ -0,0 +1,13 @@
+package punto_de_venta;
+import javax.swing.JFrame;
+import punto_de_venta.principal;
+
+public class Punto_de_venta {
+
+    
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
